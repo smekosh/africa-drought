506cbebca54b910019458f2dcfa3066a38f6cf37
Stephen Mekosh <mekosh@gmail.com>
2017-03-29 11:29:36 -0400

    Initial commit
    Moved Google sheet URL to config.php and head > style block to
    drought.css

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* config for africa-drought */
+
+
+
+// google sheet for data
+define( 'SPREADSHEET', 'https://docs.google.com/spreadsheets/d/1qsNCNKe39w6Q-rfFUxZyuDFimQklkUy-qIUUpEGKr6A/edit#gid=0' );
