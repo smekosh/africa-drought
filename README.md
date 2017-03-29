e36e949f3b1e20ea451b47f8569a8c8973517c08
Brian <bbg.visualjournalist@gmail.com>
2017-03-29 16:54:43 -0400

    VOA Boilerplate alt version.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# africa-drought
+# africa-drought #
+
+Millions of people across Africa lack access to enough food. A drought in Kenya has prompted a state of emergency. Northeast Nigeria faces acute malnutrition. Famine has been declared in parts of South Sudan, and Somalia is on the brink of famine. Tens of millions of people across the continent ― many of whom are children ― grapple with food insecurity day-to-day. Here, VOA provides ongoing coverage of this critical story.
+
+### Getting started ###
+
+If you don't have [`node-sass`](https://www.npmjs.com/package/node-sass) installed (or another way to compile SASS to CSS), you can run `sudo npm install -g node-sass`
+
+
+### What is this repository for? ###
+
+* 
+
+### To do list ###
+
+* 
+
+### Done (from the to-do list ###
+
+* 
+
+
+### Resources ###
+
+* 
---
+++ js/scripts.js
@@ -0,0 +1,196 @@
+//Grab the spreadsheet KEY from the URL bar (NOT from the published window)
+var public_spreadsheet_url = '1L_r-CsGoAOEOIBxuUBq3k8Vob-h1JEToR0w64-ehlp8';
+var map; //defined here for global access.
+
+
+
+var debugMode = true;
+// Basic function to replace console.log() statements so they can all be disabled as needed;
+function logger(logString){
+	if (debugMode){
+		console.log(logString);
+	}
+}
+
+
+
+
+
+
+
+
+// ============================
+// |  Basic tabletopJS setup  |
+// ============================
+function loadSpreadsheet() {
+	if ( mode == "editing") {
+	/*	Tabletop.init( { key: public_spreadsheet_url,
+			callback: showInfo,
+			simpleSheet: true } )
+*/
+		//multisheet version: 
+		Tabletop.init( { key: public_spreadsheet_url,
+		 	callback: showInfo,
+		 	wanted: [ "Sheet1" ] } )
+	} else if ( mode == "production") {
+		//buildPresidents(d3target);
+		showInfo(bakedData);
+	} else {
+		console.log("You need to define the 'mode' ('editing' or 'production')");
+	}
+}
+function showInfo(data, tabletop) {
+//function showInfo(data) {
+	logger("loaded spreadsheet data: ");
+	logger(data);
+
+	var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+
+
+	$.each( data.Sheet1.elements, function(i, content) {
+          var html = HRTemplate(content);
+          $("#hr").append(html);
+    })
+
+
+	/*
+	// =====================================================
+	// |  BAKE OUT THE DATA TO REMOVE TABLETOP DEPENDENCY  |
+	// =====================================================
+	if ( mode == "editing"){
+		var numberOfFeatures = data[language].elements.length;
+		var dictionary = {};
+
+		var newData = {};
+		newData[language] = {};
+		newData[language].elements = [];
+		for (var i = 0; i < numberOfFeatures; i++){
+			if ( data[language].elements[i].featureGroup == "Group"){
+				dictionary[data[language].elements[i].feature] = data[language].elements[i].translation;
+			}
+			//newData[language].elements[i] = {};
+			//newData[language].elements[i].feature =      data[language].elements[i].feature;
+			//newData[language].elements[i].featureGroup = data[language].elements[i].featureGroup;
+		}
+
+		var newDataString = JSON.stringify(newData);
+
+		console.log("----- Paste the data below at the bottom of the .html file when you're ready to publish ---------------\n\n");
+		console.log('var mode = "production";\nvar bakedData = ' + newDataString + ";\n\n");
+		var dictionaryString = JSON.stringify(dictionary);
+		console.log('var dictionary = ' + dictionaryString + ";\n")
+	}
+	*/
+
+}
+
+
+
+$(document).ready(function(){
+	logger("Ready");
+
+	// ===================
+	// |  Dropdown menu  |
+	// ===================
+	$(function() {
+		$('#main-menu').smartmenus({
+			subMenusSubOffsetX: 1,
+			subMenusSubOffsetY: -8
+		});
+	});
+
+	$( "#menuButton" ).click(function() {
+		logger("clicked menu toggle")
+	  $( ".main-menu-nav").toggle();
+	});
+
+	$( ".main-menu-nav a").not(".has-submenu").click(function() {
+	  $( ".main-menu-nav").hide();
+	});
+
+	$( ".voa__section__full-width" ).click(function() {
+	  $( ".main-menu-nav").hide();
+	});
+
+
+
+	/*
+	//=====================================================
+	// |  Add support for query strings (for languages)   |
+	//=====================================================
+	function getParameterByName(name, url) {
+		if (!url) url = window.location.href;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		    results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+
+	// you'll need to use this after the document has loaded.
+	languageQuery = getParameterByName('language');
+	if (languageQuery && languageQuery!=""){
+		language = languageQuery;
+	}
+
+	*/
+
+
+
+	/*
+	// ================================================
+	// |  Opens a pop-up with twitter sharing dialog  |
+	// ================================================
+	$('#shareTwitter').click(function(){
+		var url = "http://projects.voanews.com";
+		var text = "Replace this with your text";
+		window.open('http://twitter.com/share?url='+encodeURIComponent(url)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+	})
+	*/
+
+
+
+	// =====================================
+	// |  load spreadsheet via tabletopJS  |
+	// =====================================
+	loadSpreadsheet();
+
+
+	// =========================
+	// |  Basic Leaflet setup  |
+	// =========================
+	var path = [];
+	var marker;
+	var myLayerGroup;
+
+	//Define tileset using Mapbox
+	var mbToken = 'pk.eyJ1IjoiYndpbGxpYW1zb24iLCJhIjoiY2l0NjU5YWZhMDB0MjJ6cGd5bGU2dDd1cSJ9.4Bv8jg7AH5ksTrEvZyyjoQ';
+	var tilesetUrl = 'https://a.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}@2x.png?access_token='+mbToken;
+	var tiles = L.tileLayer(tilesetUrl, {
+		maxZoom: 18
+	});
+
+	//Create the leaflet map and restrict zoom/boundaries
+	map = L.map('map', {
+		maxZoom: 10,
+		minZoom: 2,
+		maxBounds:[
+			[35, 54],
+			[-43, -26]
+		],
+		attributionControl: false,
+		scrollWheelZoom: false,
+		layers: [tiles]
+	});
+
+	map.setView([8, 39], 4);
+	//starts map so that the continental US is centered on the screen.
+	/*map.fitBounds([
+		[35, 54],
+		[-43, -26]
+	]);
+	*/
+
+});
