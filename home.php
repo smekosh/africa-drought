4b347bb36d820e44a05830507ebecef54ccf0c2a
Salem Solomon <salemsolomon@mail.usf.edu>
2017-04-11 09:47:47 -0400

    This is the latest update to the page with some interactivity using noUiSlider to help show the levels.

--- home.php
+++ home.php
@@ -1,9 +1,19 @@
 <?php
-require( "vendor/autoload.php" );
+
+ ini_set("display_startup_errors", 1); 
+ ini_set("display_errors", 1);
+
+
+require('smarty-3.1.30/libs/Smarty.class.php');
+
+//require( "vendor/autoload.php" );
 require( "smarty_xpath.php" );
 
 $smarty = new Smarty();
 $smarty->setTemplateDir("./_timeline-editor/");
+
+
+
 
 // data source 1: timeline editor
 $data = json_decode(file_get_contents("data/slides.json"), true);
@@ -15,5 +25,7 @@ foreach( $data as $key => $value ) {
 // data source 2: google spreadsheet
 $entries = json_decode(file_get_contents("data/spreadsheet.json"), true);
 $smarty->assign( "entries", $entries );
+
+
 
 $smarty->display( '04-africa-drought.tpl' );
